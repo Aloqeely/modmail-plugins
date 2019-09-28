72bc1e520e186f4a93b9fce872dee4a79d6ac00c
DAzVise <52792999+DAzVise@users.noreply.github.com>
2019-09-28 16:32:35 +0300

    uh

--- reports/reports.py
+++ reports/reports.py
@@ -17,17 +17,17 @@ class Report(commands.Cog):
     async def reportchannel(self, ctx, channel: discord.TextChannel):
         """Set the Reports Channel"""
         await self.db.find_one_and_update(
-                {"_id": "report-config"}, {"$set": {"report_channel": channel}}
+                {"_id": "config"}, {"$set": {"report_channel": channel.name}}
             )
         await ctx.send("Successfully set the Reports channel!")
 
     @commands.command()
     async def report(self, ctx, user: discord.Member, *, reason):
         """Report a user"""
-        config = await self.db.find_one({"_id": "report-config"})
+        config = await self.db.find_one({"_id": "config"})
         report_channel = config["report_channel"]
-        if report_channel is None:
-            report_channel = discord.utils.get(ctx.guild.channels, name="reports")
+        setchannel = discord.utils.get(ctx.guild.channels, name=report_channel)
+        defaultchannel = discord.utils.get(ctx.guild.channels, name="reports")
         embed = discord.Embed(
                     color=discord.Color.red())
         embed.set_author(name=f"{ctx.author.name}",icon_url=ctx.author.avatar_url)
@@ -40,7 +40,12 @@ class Report(commands.Cog):
                 name="Channel", value=f"{ctx.channel.mention}",inline=False)
         embed.add_field(
                 name="Reason", value=reason,inline=False)
-        await report_channel.send(embed=embed)
+        try:
+            await setchannel.send(embed=embed)
+            await ctx.send("Succesfully Reported the User!")
+        except:
+            await defaultchannel.send(embed=embed)
+            await ctx.send("Succesfully Reported the User!")
                          
 def setup(bot):
     bot.add_cog(Report(bot))
