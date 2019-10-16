bb07fee0269f3b9921b76b496d8c711f68e9a6a3
DAzVise <52792999+DAzVise@users.noreply.github.com>
2019-10-16 17:15:25 +0300

    format

--- reaction-role/reaction-role.py
+++ reaction-role/reaction-role.py
@@ -29,70 +29,64 @@ class ReactionRoles(commands.Cog):
         
     @reactionrole.command()
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def add(self, ctx, channel: discord.TextChannel, role: discord.Role, emoji: typing.Union[discord.PartialEmoji, UnicodeEmoji]):
+    async def add(self, ctx, msg_id: int, role: discord.Role, emoji: typing.Union[discord.PartialEmoji, UnicodeEmoji]):
         """Sets Up the Reaction Role
-        **Note**: the reaction role **ONLY** works for one channel, you **Cannot** set it for multiple channels!
         """
-        await ctx.send("Send the Message ID of the message that you want to hold the reaction role")
-        
-        def check(id: int):
-            return id.author == ctx.author and id.channel == ctx.channel
 
-        try:
-            id = await self.bot.wait_for('message', check=check, timeout=60)
-        except asyncio.TimeoutError:
-            return await ctx.send('Reaction Role Canceled!')
-        
+        for channel in ctx.guild.TextChannels:
+            try:
+                msg = await channel.fetch_message(msg_id)
+            except:
+                pass
         await self.db.find_one_and_update(
-                {"_id": "config"}, {"$set": {str(emoji.id): role.id}}, upsert=True
+                {"_id": "config"}, {"$set": {str(emoji.id): {"role": role.id, "msg_id": msg_id}}}, upsert=True
             )
-        await self.db.find_one_and_update(
-                {"_id": "config"}, {"$set": {"rr_channel": channel.id}}, upsert=True
-            )         
-
-        msg = await channel.fetch_message(int(id.content))
         await msg.add_reaction(emoji)
         await ctx.send("Successfuly set the Reaction Role!")
         
     @reactionrole.command(aliases=["delete"])
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def remove(self, ctx, role: discord.Role, emoji: typing.Union[discord.PartialEmoji, UnicodeEmoji]):
+    async def remove(self, ctx, emoji: typing.Union[discord.PartialEmoji, UnicodeEmoji]):
         """remove something from the reaction-role
         """
-        config = await self.db.find_one({"_id": "config"})
         
-        if config is None:
-            return await ctx.send("successfully removed the role from the reaction-role")
-        
-        await self.db.delete_one({str(emoji.id): role.id})
+        await self.db.find_one_and_update({"_id": "config"}, {"$unset": {str(emoji.id): ""})
 
-        await ctx.send("successfully removed the role from the reaction-role")
+        await ctx.send("Successfully removed the role from the reaction-role")
 
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         config = await self.db.find_one({"_id": "config"})
-        channel_id = config["rr_channel"]
-        if payload.channel_id == int(channel_id):
-            guild = discord.utils.get(self.bot.guilds, id=payload.guild_id)
-            rrole = config[str(payload.emoji.id)]
+        try:
+            msg_id = config[str(payload.emoji.id)]["msg_id"]
+        except KeyError:
+            return
+                                                              
+        if payload.message_id == int(msg_id):
+            guild = self.bot.get_guild(payload.guild_id)
+            rrole = config[str(payload.emoji.id)]["role"]
             role = discord.utils.get(guild.roles, id=int(rrole))
 
             if role is not None:
-                member = discord.utils.get(guild.members, id=payload.user_id)
+                member = self.bot.get_user(payload.user_id)
                 await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
         config = await self.db.find_one({"_id": "config"})
-        channel_id = config["rr_channel"]
-        if payload.channel_id == int(channel_id):
-            guild = discord.utils.get(self.bot.guilds, id=payload.guild_id)
-            rrole = config[str(payload.emoji.id)]
+        try:
+            msg_id = config[str(payload.emoji.id)]["msg_id"]
+        except KeyError:
+            return
+                                                              
+        if payload.message_id == int(msg_id):
+            guild = self.bot.get_guild(payload.guild_id)
+            rrole = config[str(payload.emoji.id)]["role"]
             role = discord.utils.get(guild.roles, id=int(rrole))
 
             if role is not None:
-                member = discord.utils.get(guild.members, id=payload.user_id)
+                member = self.bot.get_user(payload.user_id)
                 await member.remove_roles(role)
 
                 
