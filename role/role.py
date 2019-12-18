e9fccbcf117ff1fce8e61fb2cee5564379041e1b
DAzVise <52792999+DAzVise@users.noreply.github.com>
2019-12-18 19:55:57 +0300

    quick fix

--- role/role.py
+++ role/role.py
@@ -34,19 +34,15 @@ class Role(commands.Cog):
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def createrole(self, ctx, name, color: str=None):
         """create a role."""
-        valid = re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", color)
-
-        if not valid:
-            embed = discord.Embed(
-                title="Failure",
-                description="Please enter a **valid** [hex code](https://htmlcolorcodes.com/color-picker)",
-                color=self.bot.main_color,
-            )
-
+        if color:
+            valid = re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", color)
+            if not valid:
+                embed = discord.Embed(title="Failure", color=self.bot.main_color,
+                    description="Please enter a **valid** [hex code](https://htmlcolorcodes.com/color-picker)")
             return await ctx.send(embed=embed)
+            color = discord.Color(int(color.replace("#", "0x"), 0))
 
-        color = color.replace("#", "0x")
-        await ctx.guild.create_role(name=name, color=discord.Color(int(color, 0)))
+        await ctx.guild.create_role(name=name, color=color)
         await ctx.send("Successfully created the role!")
 
 def setup(bot):
