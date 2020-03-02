23de981b3d316d53b9d8b95f55e6c80476eac22c
DAzVise <52792999+DAzVise@users.noreply.github.com>
2020-03-02 21:37:33 +0300

    fix issue #1115

--- role/role.py
+++ role/role.py
@@ -32,18 +32,22 @@ class Role(commands.Cog):
 
     @commands.command(aliases=["makerole"])
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def createrole(self, ctx, name, color: str=None):
+    async def createrole(self, ctx, name, color: str):
         """create a role."""
-        if color:
-            valid = re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", color)
-            if not valid:
-                embed = discord.Embed(title="Failure", color=self.bot.main_color,
-                    description="Please enter a **valid** [hex code](https://htmlcolorcodes.com/color-picker)")
+        valid = re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", color)
+        if not valid:
+            embed = discord.Embed(title="Failure", color=self.bot.main_color,
+                description="Please enter a **valid** [hex code](https://htmlcolorcodes.com/color-picker)")
             return await ctx.send(embed=embed)
-            color = discord.Color(int(color.replace("#", "0x"), 0))
 
-        await ctx.guild.create_role(name=name, color=color)
+        await ctx.guild.create_role(name=name, color=int(color.replace("#", "0x"), 0))
         await ctx.send("Successfully created the role!")
+        
+    @createrole.error
+    async def createrole_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(title="No tag provided!", description="Please provide your player tag.", color=0xFF0000)
+            return await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Role(bot))
