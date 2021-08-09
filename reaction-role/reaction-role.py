e721914f28d4b85197bdd0776cdd90fc2c12183d
DAzVise <52792999+DAzVise@users.noreply.github.com>
2021-08-09 18:25:02 +0300

    Fix some emojis not being detected

--- reaction-role/reaction-role.py
+++ reaction-role/reaction-role.py
@@ -11,6 +11,7 @@ from core.models import PermissionLevel
 
 class UnicodeEmoji(commands.Converter):
     async def convert(self, ctx, argument):
+        argument = emoji.emojize(emoji.demojize(argument))
         if argument in emoji.UNICODE_EMOJI["en"]:
             return discord.PartialEmoji(name=argument, animated=False)
         raise commands.BadArgument('Unknown emoji')
